b4fa5c6fc22211541ac174256ad9f5661044b799
Alexander Tiedmann <alexander.tiedmann@googlemail.com>
2016-12-14 08:27:08 +0100

    no message

--- Beispiel-Scripte/06_ki67analysis_iterationBenchmark.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-{ while true ; do { for i in "4"; do time { printf "$(date)\t${i}\t" && ./05_ki67analysis_executing.sh ${i}| tail -n 1;  }   ; done; } done;} | tee -a scores.log
--- Beispiel-Scripte/pingPluto.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-time { for i in $(seq 22); do echo -n "pluto${i}: "; ping -t 1 -Q -o -c 1 pluto${i}.f4.htw-berlin.de | head -n 2 | tail -n 1 ; done }
-
